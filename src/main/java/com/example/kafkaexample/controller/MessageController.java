bbf6528cf4ff7d7a820783e82efa870b24939fe3
joaokage1 <j_vgomes@hotmail.com>
2022-12-11 18:01:35 -0300

    Kafka consumer - consuming

---
+++ src/main/java/com/example/kafkaexample/controller/MessageController.java
@@ -0,0 +1,22 @@
+package com.example.kafkaexample.controller;
+
+import com.example.kafkaexample.dto.MessageRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/messages")
+@RequiredArgsConstructor
+public class MessageController {
+
+    private final KafkaTemplate<String, String> kafkaTemplate;
+    @PostMapping
+    public void publish(@RequestBody MessageRequest request){
+        kafkaTemplate.send("kafka-topic-1",request.message());
+    }
+}
---
+++ src/main/java/com/example/kafkaexample/dto/MessageRequest.java
@@ -0,0 +1,3 @@
+package com.example.kafkaexample.dto;
+
+public record MessageRequest(String message){}
