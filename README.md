fd7bdc530fd4f6b59e914ae53d2ad2795c12b0c7
joaokage1 <j_vgomes@hotmail.com>
2022-12-11 17:24:11 -0300

    Initial config - Kafka topic config

---
+++ README.md
@@ -0,0 +1,5 @@
+# Kafka example
+This example was built thinking on a Kafka broker running on localhost with port 9092.
+
+>Please follow the steps on:
+https://kafka.apache.org/documentation/#quickstart
---
+++ src/main/java/com/example/kafkaexample/config/KafkaTopicConfig.java
@@ -0,0 +1,15 @@
+package com.example.kafkaexample.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaTopicConfig {
+
+    @Bean
+    public NewTopic kafkaExampleTopic() {
+        return TopicBuilder.name("kafka-topic-1").build();
+    }
+}
